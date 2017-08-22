b831ff14be93b6a1251eadf586937fdeb8d551c9
avermeulen <andre.vermeulen@gmail.com>
2017-08-22 15:13:09 +0200

    removed code

--- know-they-neighbour.js
+++ know-they-neighbour.js
@@ -1,15 +1,4 @@
 module.exports = function(list){
-    for (var i = 0; i < list.length; i++) {
-        var current = list[i];
-        var next = list[i+1];
-        if (!next){
-            break;
-        }
-        if (current > next){
-            let storedValue = next;
-            list[i+1] = current;
-            list[i] = storedValue;
-        }
-    }
-    return list;
+    
+    return [];
 }
--- swap-first-and-last.js
+++ swap-first-and-last.js
@@ -1,12 +1,3 @@
 module.exports = function(list){
-
-    var first = list[0];
-    var last = list[list.length-1];
-
-    list[0] = last;
-    list[list.length -1] = first
-
-    return list;
-
-    //return [];
+    return [];
 }
